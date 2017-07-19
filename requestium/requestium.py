d7ef867586d8298a1cf956631be1409fce7e7697
Joaquin Alori <joaquin.alori@gmail.com>
2017-07-19 15:47:11 -0300

    Add support for more robust waits and clicks in selenium driver

--- requestium/requestium.py
+++ requestium/requestium.py
@@ -3,8 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from parsel.selector import Selector
+import time
 import tldextract
+from functools import partial
 
 
 class Session(requests.Session):
@@ -22,7 +25,7 @@ class Session(requests.Session):
     _driver = None
     _last_requests_url = None
 
-    def __init__(self, webdriver_path='./phantomjs_driver', default_timeout=5, browser='phantomjs'):
+    def __init__(self, webdriver_path='./phantomjs', default_timeout=5, browser='phantomjs'):
         super(Session, self).__init__()
         self.webdriver_path = webdriver_path
         self.default_timeout = default_timeout
@@ -41,7 +44,7 @@ class Session(requests.Session):
                 )
 
             # Add useful method to driver
-            self.driver.wait_for_xpath = self.__wait_for_xpath
+            self.driver.ensure_element_by_xpath = self.__ensure_element_by_xpath
         return self._driver
 
     def _start_phantomjs_browser(self, webdriver_path):
@@ -72,8 +75,15 @@ class Session(requests.Session):
     def _start_chrome_browser(self):
         # TODO transfer headers, and authenticated proxyes: not sure how to do it in chrome
 
+        chrome_options = webdriver.chrome.options.Options()
+
+        # The infobar at the top saying 'Chrome is being controlled by an automated software'
+        # sometimes hide elements from being clickable! So we disable it.
+        chrome_options.add_argument('disable-infobars')
+
+
         # Create driver process
-        self._driver = webdriver.Chrome(self.webdriver_path)
+        self._driver = webdriver.Chrome(self.webdriver_path, chrome_options=chrome_options)
 
     def update_driver_cookies(self, url=None):
         """Copies the Session's cookies into the webdriver
@@ -122,10 +132,11 @@ class Session(requests.Session):
                 return url
         return components.registered_domain
 
-    def __wait_for_xpath(self, selector, criterium="presence", timeout=None):
-        """This method allows us to wait till an element is loaded in selenium.
+    def __ensure_element_by_xpath(self, selector, criterium="presence", timeout=None):
+        """This method allows us to wait till an element is loaded in selenium
 
-        This method is added to the driver object.
+        This method is added to the driver object. And its more robust than any of Selenium's
+        default options for waiting.
 
         Selenium runs in parallel with our scripts, so we must wait for it everytime it
         runs javascript. Selenium automatically makes our python scripts when its GETing
@@ -140,18 +151,47 @@ class Session(requests.Session):
         This is a barebones implementation, which only supports xpath. It could be usefull to
         add more filters in the future, a comprehensive list of the possible filters can be
         found here: http://selenium-python.readthedocs.io/waits.html
+
+        This function returns the element its waiting for, so it could actually replace
+        the default selenium method 'find_element_by_xpath'. I am not doing it for the time being
+        as it could cause confusion and have some adverse effects I may not not be aware of,
+        but its worth considering doing it when this whole library is more stable and we have a
+        better defined api.
+
+        This function also scrolls the element into view before returning it, so we can ensure that
+        the element is clickable before returning it.
         """
         type = By.XPATH
         if not timeout: timeout = self.default_timeout
 
         if criterium == 'visibility':
-            return WebDriverWait(self._driver, timeout).until(
+            element = WebDriverWait(self._driver, timeout).until(
                 EC.visibility_of_element_located((type, selector))
             )
-        else:
-            return WebDriverWait(self._driver, timeout).until(
+        elif criterium == 'clickable':
+            element = WebDriverWait(self._driver, timeout).until(
+                EC.element_to_be_clickable((type, selector))
+            )
+        elif criterium == 'presence':
+            element = WebDriverWait(self._driver, timeout).until(
                 EC.presence_of_element_located((type, selector))
             )
+        else:
+            raise ValueError(
+                "The 'criterium' argument must be 'visibility', 'clickable' "
+                "or 'presence', not '{}'".format(criterium)
+            )
+
+        # This next method returns the location of an element once its scrolled into view.
+        # It scrolls the element into view first though, so its an effective way to ensure
+        # the element is viewable when we return it in case we want to click it.
+        element.location_once_scrolled_into_view
+
+        # We add this method to our element to provide a more robust click. Chromedriver
+        # sometimes needs some time before it can click an item, specially if it needs to
+        # scroll into it first. This method ensures clicks don't fail because of this.
+        element.ensure_click = partial(_ensure_click, element)
+        return element
 
     def get(self, *args, **kwargs):
         resp = super(Session, self).get(*args, **kwargs)
@@ -197,3 +237,26 @@ class RequestiumResponse(object):
 
     def re_first(self, *args, **kwargs):
         return self.selector.re_first(*args, **kwargs)
+
+
+def _ensure_click(self):
+    """Ensures a click gets made even when using the buggy chromedriver
+
+    This method gets added to the selenium elemenent returned in '__ensure_element_by_xpath'.
+
+    I wrote this method out of frustration with chromedriver and its problems with clicking
+    items that need to be scrolled to in order to be clickable. In '__ensure_element_by_xpath' we
+    scroll to the item before returning it, but chrome has some problems if it doesn't get some
+    time to scroll to the item. This method ensures chromes gets enough time to scroll to the item
+    before clicking it. I tried SEVERAL more 'correct' methods to get around this, but none of them
+    worked 100% of the time. Checking if the item is 'clickable' does not work.
+    """
+    for _ in range(5):
+        try:
+            self.click()
+            return
+        except WebDriverException as e:
+            time.sleep(0.2)
+    raise WebDriverException(
+        "Couldn't click item after trying 5 times, got error message: \n{}".format(e.message)
+    )
