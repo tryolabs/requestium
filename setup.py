0e1f5f630ab8943212b93c1e1751e3cd14ddbd01
Wil Thieme <wil.thieme@protonmail.com>
2025-01-09 12:53:25 -0500

    update module version number to 0.5.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as file:
 
 setup(
     name="requestium",
-    version="0.4.1",
+    version="0.5.0",
     description=(
         "Adds a Selenium webdriver and parsel's parser to a request's Session "
         "object, and makes switching between them seamless. Handles cookie, "
