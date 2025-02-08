28b6f3bdd448a17725f2fd4561a9f418acd10b23
Wil T <wil.t.me@pm.me>
2025-02-08 08:52:43 -0500

    ruff fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 # Get the long description from the README file
-with open('README.md') as file:
+with open("README.md") as file:
     long_description = file.read()
 
 setup(
@@ -14,25 +14,25 @@ setup(
         "proxy and header transfer."
     ),
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Joaquin Alori',
-    author_email='joaquin@tryolabs.com',
-    url='https://github.com/tryolabs/requestium',
-    packages=('requestium',),
+    long_description_content_type="text/markdown",
+    author="Joaquin Alori",
+    author_email="joaquin@tryolabs.com",
+    url="https://github.com/tryolabs/requestium",
+    packages=("requestium",),
     install_requires=(
-        'parsel>=1.7.0',
-        'requests>=2.28.1',
-        'selenium>=4.6.0',
-        'tldextract>=3.4.0',
+        "parsel>=1.7.0",
+        "requests>=2.28.1",
+        "selenium>=4.6.0",
+        "tldextract>=3.4.0",
     ),
-    license='MIT',
+    license="MIT",
     zip_safe=False,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
     ],
 )
