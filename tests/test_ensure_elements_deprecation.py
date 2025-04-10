0226042e0762eed29201b951f9c3e2cdea833132
Wil T <wil.t.me@pm.me>
2025-04-09 22:16:57 -0400

    remove tempfile for user-data-dir

--- tests/test_ensure_elements_deprecation.py
+++ tests/test_ensure_elements_deprecation.py
@@ -1,6 +1,5 @@
 import contextlib
 import shutil
-import tempfile
 
 import pytest
 import selenium.webdriver
@@ -9,21 +8,19 @@ from requestium import Session
 
 @contextlib.contextmanager
 def create_chrome_session(headless=False):
-    with tempfile.TemporaryDirectory() as tmp_profile_dir:
-        chrome_options = selenium.webdriver.ChromeOptions()
-        if headless:
-            chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument(f"--user-data-dir={tmp_profile_dir}")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options = selenium.webdriver.ChromeOptions()
+    if headless:
+        chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-        session = Session(
-            webdriver_path=shutil.which("chromedriver"), browser_options=chrome_options
-        )
-        try:
-            yield session
-        finally:
-            session.driver.quit()
+    session = Session(
+        webdriver_path=shutil.which("chromedriver"), browser_options=chrome_options
+    )
+    try:
+        yield session
+    finally:
+        session.driver.quit()
 
 
 session_factories = {
