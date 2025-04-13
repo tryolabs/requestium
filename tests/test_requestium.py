b9d8e773d96876b6796bb3a4915620eaa0b7c38b
Wil T <wil.t.me@pm.me>
2025-04-13 14:53:32 -0400

    remove session fixture from test_requestium in favor of per-test sessions with context managers

--- tests/test_requestium.py
+++ tests/test_requestium.py
@@ -1,7 +1,4 @@
 import shutil
-from typing import Any
-
-from collections.abc import Generator
 
 import pytest
 import selenium
@@ -9,7 +6,6 @@ from selenium.common import InvalidCookieDomainException
 from selenium.webdriver.common.by import By
 
 import requestium
-from requestium import Session
 
 chrome_webdriver_path = shutil.which("chromedriver")
 
@@ -24,37 +20,40 @@ session_parameters = [
 ]
 
 
-@pytest.fixture(params=session_parameters)
-def session(request) -> Generator[Session, Any, None]:
-    session = requestium.Session(**request.param)
-    yield session
-    session.driver.quit()
+@pytest.mark.parametrize("params", session_parameters)
+def test_simple_page_load(params) -> None:
+    with requestium.Session(**params) as session:
+        session.driver.get("http://the-internet.herokuapp.com")
+        session.driver.ensure_element(By.ID, "content")
+
+        title = session.driver.title
+        heading = session.driver.find_element(By.XPATH, '//*[@id="content"]/h1')
+
+        assert title == "The Internet"
+        assert heading.text == "Welcome to the-internet"
 
 
-def test_simple_page_load(session) -> None:
-    session.driver.get("http://the-internet.herokuapp.com")
-    session.driver.ensure_element(By.ID, "content")
-    title = session.driver.title
-    heading = session.driver.find_element(By.XPATH, '//*[@id="content"]/h1')
-    assert title == "The Internet"
-    assert heading.text == "Welcome to the-internet"
-
-
-def test_copy_user_agent_from_driver(session) -> None:
+@pytest.mark.parametrize("params", session_parameters)
+def test_copy_user_agent_from_driver(params) -> None:
     """Ensure that requests user-agent header has been changed after calling session.copy_user_agent_from_driver()"""
-    pre_copy_requests_useragent = session.headers["user-agent"]
-    assert pre_copy_requests_useragent and pre_copy_requests_useragent != ""
+    with requestium.Session(**params) as session:
+        pre_copy_requests_useragent = session.headers["user-agent"]
+        assert pre_copy_requests_useragent and pre_copy_requests_useragent != ""
 
-    session.driver.get("http://example.com")
-    session.copy_user_agent_from_driver()
-    post_copy_requests_useragent = session.headers["user-agent"]
+        session.driver.get("http://the-internet.herokuapp.com")
+        session.copy_user_agent_from_driver()
+        post_copy_requests_useragent = session.headers["user-agent"]
 
-    assert post_copy_requests_useragent != pre_copy_requests_useragent
+        assert post_copy_requests_useragent != pre_copy_requests_useragent
 
 
-def test_transfer_session_cookies_to_driver_no_domain_error(session) -> None:
-    with pytest.raises(
-        InvalidCookieDomainException,
-        match="Trying to transfer cookies to selenium without specifying a domain and without having visited any page in the current session",
+@pytest.mark.parametrize("params", session_parameters)
+def test_transfer_session_cookies_to_driver_no_domain_error(params) -> None:
+    with (
+        requestium.Session(**params) as session,
+        pytest.raises(
+            InvalidCookieDomainException,
+            match="Trying to transfer cookies to selenium without specifying a domain and without having visited any page in the current session",
+        ),
     ):
         session.transfer_session_cookies_to_driver()
