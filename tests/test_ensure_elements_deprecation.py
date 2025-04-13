16246e57ad287bb2613f638375b1a19e01746b49
Wil T <wil.t.me@pm.me>
2025-04-13 15:19:17 -0400

    don't parameterize test_deprecation_warning_for_ensure_element_locators_with_underscores

--- tests/test_ensure_elements_deprecation.py
+++ tests/test_ensure_elements_deprecation.py
@@ -1,12 +1,9 @@
 import pytest
 import requestium
 
-from test_requestium import session_parameters
 
-
-@pytest.mark.parametrize("params", session_parameters)
-def test_deprecation_warning_for_ensure_element_locators_with_underscores(params) -> None:
-    with requestium.Session(**params) as session:
+def test_deprecation_warning_for_ensure_element_locators_with_underscores() -> None:
+    with requestium.Session() as session:
         session.driver.get("http://the-internet.herokuapp.com")
         with pytest.warns(DeprecationWarning):
             session.driver.ensure_element("class_name", "no-js")
