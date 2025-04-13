a40eaf06066bdc00932f1a1d41ef5e7fbe74ac66
Wil T <wil.t.me@pm.me>
2025-04-13 14:52:52 -0400

    use session_parameters from test_requestium in test_ensure_elements_deprecation

--- tests/test_ensure_elements_deprecation.py
+++ tests/test_ensure_elements_deprecation.py
@@ -1,35 +1,12 @@
-import shutil
-from typing import Any
-
-from collections.abc import Generator
-
 import pytest
-import selenium
-
 import requestium
-from requestium import Session
 
-chrome_webdriver_path = shutil.which("chromedriver")
-
-chrome_webdriver = selenium.webdriver.chrome.webdriver.WebDriver()
-firefox_webdriver = selenium.webdriver.firefox.webdriver.WebDriver()
-
-session_parameters = [
-    {"webdriver_path": chrome_webdriver_path},
-    {"webdriver_path": chrome_webdriver_path, "headless": True},
-    {"driver": chrome_webdriver},
-    {"driver": firefox_webdriver},
-]
+from test_requestium import session_parameters
 
 
-@pytest.fixture(params=session_parameters)
-def session(request) -> Generator[Session, Any, None]:
-    session = requestium.Session(**request.param)
-    yield session
-    session.driver.quit()
-
-
-def test_deprecation_warning_for_ensure_element_locators_with_underscores(session) -> None:
-    session.driver.get("http://the-internet.herokuapp.com")
-    with pytest.warns(DeprecationWarning):
-        session.driver.ensure_element("class_name", "no-js")
+@pytest.mark.parametrize("params", session_parameters)
+def test_deprecation_warning_for_ensure_element_locators_with_underscores(params) -> None:
+    with requestium.Session(**params) as session:
+        session.driver.get("http://the-internet.herokuapp.com")
+        with pytest.warns(DeprecationWarning):
+            session.driver.ensure_element("class_name", "no-js")
