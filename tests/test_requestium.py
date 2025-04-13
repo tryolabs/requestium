b044c2cae949f93d32730c32e32ab8f242fb9eeb
Wil T <wil.t.me@pm.me>
2025-04-13 14:18:17 -0400

    use example.com for test_copy_user_agent_from_driver

--- tests/test_requestium.py
+++ tests/test_requestium.py
@@ -45,7 +45,7 @@ def test_copy_user_agent_from_driver(session) -> None:
     pre_copy_requests_useragent = session.headers["user-agent"]
     assert pre_copy_requests_useragent and pre_copy_requests_useragent != ""
 
-    session.driver.get("http://the-internet.herokuapp.com")
+    session.driver.get("http://example.com")
     session.copy_user_agent_from_driver()
     post_copy_requests_useragent = session.headers["user-agent"]
 
