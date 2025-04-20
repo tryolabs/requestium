74a2d8ba08c1b25582e1f7ed8b6576b3ccb306d1
Wil T <wil.t.me@pm.me>
2025-04-20 08:26:23 -0400

    Separate files and type hinting improvements (#94)
    * break requestium into separate files
    
    * import Session via .requestium for coverage
    
    * typing in RequestiumResponse
    
    * use selenium.webdriver.Chrome/Firefox rather than selenium.webdriver.chrome/firefox.webdriver.WebDriver
    
    * mixin type hints
    
    * response type hints
    
    * session type hints
    
    * inherit requests.Response to RequestiumResponse
    
    * improve try_add_cookie error handling
    
    * don't use global webdriver instance, create when running tests
    
    * skip chrome-custom-path tests

---
+++ requestium/requestium_response.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+import requests
+from parsel.selector import Selector, SelectorList
+from requests import Response
+
+
+class RequestiumResponse(requests.Response):
+    """Adds xpath, css, and regex methods to a normal requests response object"""
+
+    def __init__(self, response: Response) -> None:
+        super().__init__()
+        self.__class__ = type(response.__class__.__name__, (self.__class__, response.__class__), response.__dict__)
+
+    @property
+    def selector(self) -> Selector:
+        """Returns the response text in a Selector
+
+        We re-parse the text on each xpath, css, re call in case the encoding has changed.
+        """
+        return Selector(text=self.text)
+
+    def xpath(self, *args, **kwargs) -> SelectorList[Selector]:
+        return self.selector.xpath(*args, **kwargs)
+
+    def css(self, *args, **kwargs) -> SelectorList[Selector]:
+        return self.selector.css(*args, **kwargs)
+
+    def re(self, *args, **kwargs) -> list[str]:
+        return self.selector.re(*args, **kwargs)
+
+    def re_first(self, *args, **kwargs) -> Optional[str]:
+        return self.selector.re_first(*args, **kwargs)
--- tests/test_session.py
+++ tests/test_session.py
@@ -1,32 +1,12 @@
-import shutil
-
-import pytest
-import selenium
 from selenium.webdriver.common.by import By
 
-import requestium
 
-chrome_webdriver_path = shutil.which("chromedriver")
+def test_simple_page_load(session) -> None:
+    session.driver.get("http://the-internet.herokuapp.com")
+    session.driver.ensure_element(By.ID, "content")
 
-chrome_webdriver = selenium.webdriver.chrome.webdriver.WebDriver()
-firefox_webdriver = selenium.webdriver.firefox.webdriver.WebDriver()
+    title = session.driver.title
+    heading = session.driver.find_element(By.XPATH, '//*[@id="content"]/h1')
 
-session_parameters = [
-    {"webdriver_path": chrome_webdriver_path},
-    {"webdriver_path": chrome_webdriver_path, "headless": True},
-    {"driver": chrome_webdriver},
-    {"driver": firefox_webdriver},
-]
-
-
-@pytest.mark.parametrize("params", session_parameters)
-def test_simple_page_load(params) -> None:
-    with requestium.Session(**params) as session:
-        session.driver.get("http://the-internet.herokuapp.com")
-        session.driver.ensure_element(By.ID, "content")
-
-        title = session.driver.title
-        heading = session.driver.find_element(By.XPATH, '//*[@id="content"]/h1')
-
-        assert title == "The Internet"
-        assert heading.text == "Welcome to the-internet"
+    assert title == "The Internet"
+    assert heading.text == "Welcome to the-internet"
