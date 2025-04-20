6bce03d4407bc4fee250fe1135db9c743478c0a2
Wil T <wil.t.me@pm.me>
2025-04-20 14:25:25 -0400

    re-sort conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,34 +13,34 @@ if TYPE_CHECKING:
 
 @pytest.fixture(
     params=[
-        "chrome",
         "chrome-headless",
-        "firefox",
-        "firefox-headless",
+        "chrome",
         # "chrome-custom-path",
+        "firefox-headless",
+        "firefox",
     ]
 )
-def session(request):
+def session(request):  # noqa: ANN001, ANN201
     driver_type = request.param
 
-    if driver_type == "chrome":
-        driver = webdriver.Chrome()
-    elif driver_type == "chrome-headless":
+    if driver_type == "chrome-headless":
         options = webdriver.ChromeOptions()
         options.add_argument("--headless=new")
         driver = webdriver.Chrome(options=options)
-    elif driver_type == "firefox":
-        driver = webdriver.Firefox()
-    elif driver_type == "firefox-headless":
-        options = webdriver.FirefoxOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Firefox(options=options)
+    elif driver_type == "chrome":
+        driver = webdriver.Chrome()
     # elif driver_type == "chrome-custom-path":
     #     chromedriver_name = "chromedriver"
     #     custom_path = shutil.which(chromedriver_name)
     #     assert custom_path, f"'{chromedriver_name}' not found in PATH."
     #     assert os.path.exists(custom_path), f"Custom chromedriver not found at {custom_path}."
     #     driver = webdriver.Chrome(service=webdriver.ChromeService(executable_path=custom_path))
+    elif driver_type == "firefox-headless":
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+    elif driver_type == "firefox":
+        driver = webdriver.Firefox()
     else:
         msg = f"Unknown driver type: {driver_type}"
         raise ValueError(msg)
