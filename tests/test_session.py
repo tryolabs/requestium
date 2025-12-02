f173c18b9e73bde70949cf96596ce1add612f986
Ramona T <ramona.t.me@pm.me>
2025-12-02 17:14:42 -0500

    create test_mixin

--- tests/test_session.py
+++ tests/test_session.py
@@ -8,17 +8,6 @@ from selenium.webdriver.common.by import By
 import requestium.requestium
 
 
-def test_simple_page_load(session: requestium.Session, example_html: str) -> None:
-    session.driver.get(f"data:text/html,{example_html}")
-    session.driver.ensure_element(By.TAG_NAME, "h1")
-
-    title = session.driver.title
-    heading = session.driver.find_element(By.TAG_NAME, "h1")
-
-    assert title == "The Internet"
-    assert heading.text == "Test Page"
-
-
 @pytest.mark.parametrize(
     "headless",
     [
