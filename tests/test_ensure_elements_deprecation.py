566b7325e5ee7f31e3785ce5c9e570c3b91c8a53
Ramona T <ramona.t.me@pm.me>
2025-12-02 17:39:34 -0500

    add parameterized test_ensure_element

--- tests/test_ensure_elements_deprecation.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-import requestium.requestium
-
-
-def test_deprecation_warning_for_ensure_element_locators_with_underscores(session: requestium.Session, example_html: str) -> None:
-    session.driver.get(f"data:text/html,{example_html}")
-    with pytest.warns(DeprecationWarning):
-        session.driver.ensure_element("tag_name", "h1")
