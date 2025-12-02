eb42547c00f0415abb304c8c71e6fee1273eaa5e
Ramona T <ramona.t.me@pm.me>
2025-12-02 14:20:36 -0500

    add test for domain filter for session to driver cookie transfer

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -38,6 +38,21 @@ def test_transfer_session_cookies_to_driver(session: requestium.Session, cookie_
     assert driver_cookies[0]["path"] == cookie_data["path"]
 
 
+def test_transfer_session_cookies_to_driver_domain_filter(session: requestium.Session, cookie_data: dict[str, str]) -> None:
+    session.get(f"http://{cookie_data['domain']}")
+    session.cookies.set(name="junk_cookie", value="sfkjn782", domain="google.com", path=cookie_data["path"])
+    session.cookies.set(name=cookie_data["name"], value=cookie_data["value"], domain=cookie_data["domain"], path=cookie_data["path"])
+
+    assert session.driver.get_cookies() == []
+    session.transfer_session_cookies_to_driver(domain=cookie_data["domain"])
+    driver_cookies = session.driver.get_cookies()
+    assert len(driver_cookies) == 1
+    assert driver_cookies[0]["name"] == cookie_data["name"]
+    assert driver_cookies[0]["value"] == cookie_data["value"]
+    assert driver_cookies[0]["domain"] in [cookie_data["domain"], f".{cookie_data['domain']}"]
+    assert driver_cookies[0]["path"] == cookie_data["path"]
+
+
 def test_transfer_session_cookies_to_driver_no_domain_error(session: requestium.Session) -> None:
     with (
         pytest.raises(
